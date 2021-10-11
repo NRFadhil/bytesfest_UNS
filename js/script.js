0c1c4264138ff6deb72b074d7239d0ebef29a8eb
Nalan <nalanfadhilah1@gmail.com>
2021-10-11 09:07:15 +0700

    login

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.sliderBerita').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="bi bi-arrow-left-circle">',
+  nextArrow: '<i class="bi bi-arrow-right-circle">',
+
+});
